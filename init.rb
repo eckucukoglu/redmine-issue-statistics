f0e4142ac8b2dd63a51d288671e6e036dabd7266
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-07-14 02:22:21 +0300

    initial commit

---
+++ init.rb
@@ -0,0 +1,12 @@
+Redmine::Plugin.register :issuestats do
+  name 'Issue Statistics'
+  url 'https://github.com/eckucukoglu/redmine-issue-statistics'
+  description 'Issue statistics'
+  author 'Emre Can Kucukoglu'
+  author_url 'http://eckucukoglu.com'
+  version '0.0.1'
+  requires_redmine version_or_higher: '2.5.2'
+
+  
+
+end
