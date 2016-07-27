643ecbc8108c1e226f1c6d0d3a54d538ac58fb59
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-07-27 10:21:15 +0300

    foreign key removed.

--- db/migrate/001_create_issue_start_times.rb
+++ db/migrate/001_create_issue_start_times.rb
@@ -1,7 +1,7 @@
 class CreateIssueStartTimes < ActiveRecord::Migration
   def change
     create_table :issue_start_times do |t|
-      t.references :issue, index: true, foreign_key: true, null: false
+      t.references :issue, null: false
       t.datetime :start_datetime, null: false
     end
   end
